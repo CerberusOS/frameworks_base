2fef99b6c50cfc5ad7124f7accf537fb3dcfb2f2
paphonb <gitwahubi.com/paphonb>
2019-05-12 12:29:33 +0530

    base: Add Q style battery icon toggle [1/2]

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -55,6 +55,7 @@ public class BatteryMeterDrawableBase extends Drawable {
     public static final int BATTERY_STYLE_TEXT = 5;
     public static final int BATTERY_STYLE_HIDDEN = 6;
     public static final int BATTERY_STYLE_BIG_DOTTED_CIRCLE = 7;
+    public static final int BATTERY_STYLE_Q = 8;
 
     protected final Context mContext;
     protected final Paint mFramePaint;
@@ -687,7 +688,7 @@ public class BatteryMeterDrawableBase extends Drawable {
     }
 
     protected float getAspectRatio() {
-        if (mMeterStyle != BATTERY_STYLE_PORTRAIT) {
+        if (mMeterStyle != BATTERY_STYLE_PORTRAIT && mMeterStyle != BATTERY_STYLE_Q) {
             return CIRCLE_ASPECT_RATIO;
         }
         return mShowPercent && !mCharging ? ASPECT_RATIO_PERCENTAGE_INSIDE : ASPECT_RATIO;
